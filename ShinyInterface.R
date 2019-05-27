010a94d57048e5854fe07d4c687ef4d158b5be95
Unknown <matmg@interia.eu>
2019-05-27 12:40:22 +0200

    Test Shiny Controlers 1

--- ShinyInterface.R
+++ ShinyInterface.R
@@ -1,6 +1,14 @@
 library(shiny)
+library(shinyWidgets)
+library(ggplot2) 
 source("UI.R")
 source("Server.R")
+# choose columns to display
 
+smoke <- matrix(c(51,43,22,92,28,21,68,22,9),ncol=3,byrow=TRUE)
+colnames(smoke) <- c("Symbol","Id","Opis")
+rownames(smoke) <- c("Gen1","Gen2","Gen3")
+smoke <- as.table(smoke)
+prop.table(smoke)
+#shinyApp(ui=ui,server=server)
 
-shinyApp(ui=ui,server=server)
--- UI.R
+++ UI.R
@@ -1,26 +1,29 @@
 library(shiny)
+library(DT)
+s = c("set1", "set2", "set3", "set4", "set5") 
 ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Tabsets"),
-  
-  # Sidebar layout with input and output definitions ----
+  title = "Examples of DataTables",
   sidebarLayout(
-    
-    
     sidebarPanel(
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Tabset w/ plot, summary, and table ----
-      tabsetPanel(type = "tabs",
-                  tabPanel("Geny", plotOutput("Gens") , tableOutput('table')),
-                  tabPanel("Summary", verbatimTextOutput("summary")),
-                  tabPanel("Table", tableOutput("table"))
+      panel("Content goes here",
+        checkboxGroupInput("show_vars", "Loaded set",
+                           names(diamonds), selected = names(diamonds))
       )
-  )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel("Tabela genów", DT::dataTableOutput("mytable1")),
+        tabPanel("Wczytywanie plików",fileInput("file1", "Wybierz pli CSV",
+                                       accept = c(
+                                         "text/csv",
+                                         "text/comma-separated-values,text/plain",
+                                         ".csv")), actionButton("button", "Dodaj zbiór")),
+        tabPanel("Wykresy", selectInput("Ind","Indipendent Variable",choices = names(mtcars)),
+                 selectInput('Dep','  Dependent Variable',choices = names(mtcars)),
+                 plotOutput("BoxPlot"),
+                 plotOutput('Hist'))
+      )
+    )
   )
 )
